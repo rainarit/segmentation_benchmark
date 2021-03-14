a81dea11c5e9e77d7e93a4cdf242fa31eb67ae67
rainarit <rraina@ucsd.edu>
2021-03-13 23:37:00 -0800

    torchbench-addition

--- torchbench_PASCALVOC.py
+++ torchbench_PASCALVOC.py
@@ -1,34 +1,221 @@
-from torchbench.semantic_segmentation import PASCALVOC
+from __future__ import print_function
+from collections import defaultdict, deque
+import datetime
+import math
+import time
+import torch
+import torch.distributed as dist
+from torchvision import transforms as T
+from torchvision.transforms import functional as F
+import torch.utils.data
+from torch import nn
+import torchvision
 from torchbench.semantic_segmentation.transforms import (
     Normalize,
     Resize,
     ToTensor,
     Compose,
 )
-from torchvision.models.segmentation import fcn_resnet101
-import torchvision.transforms as transforms
-import PIL
+import numpy as np
+from PIL import Image
+import random
+import errno
+import os
 
-def model_output_function(output, labels):
-    return output['out'].argmax(1).flatten(), target.flatten()
+import MetricLogger
+import SmoothedValue
+import ConfusionMatrix
 
-def seg_collate_fn(batch):
+MODEL_NAME = 'fcn_resnet101'
+model = torchvision.models.segmentation.__dict__['fcn_resnet101'](num_classes=21, pretrained=True)
+
+def cat_list(images, fill_value=0):
+    max_size = tuple(max(s) for s in zip(*[img.shape for img in images]))
+    batch_shape = (len(images),) + max_size
+    batched_imgs = images[0].new(*batch_shape).fill_(fill_value)
+    for img, pad_img in zip(images, batched_imgs):
+        pad_img[..., :img.shape[-2], :img.shape[-1]].copy_(img)
+    return batched_imgs
+
+
+def collate_fn(batch):
     images, targets = list(zip(*batch))
     batched_imgs = cat_list(images, fill_value=0)
     batched_targets = cat_list(targets, fill_value=255)
     return batched_imgs, batched_targets
 
-model = fcn_resnet101(num_classes=21, pretrained=True)
 
-normalize = Normalize(
-    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-)
-my_transforms = Compose([Resize((520, 480)), ToTensor(), normalize])
+def mkdir(path):
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
 
-PASCALVOC.benchmark(batch_size=32,
-    model=model,
-    transforms=my_transforms,
-    model_output_transform=model_output_function,
-    collate_fn=seg_collate_fn,
-    paper_model_name='FCN ResNet-101',
-    paper_arxiv_id='1605.06211')
+
+def setup_for_distributed(is_master):
+    """
+    This function disables printing when not in master process
+    """
+    import builtins as __builtin__
+    builtin_print = __builtin__.print
+
+    def print(*args, **kwargs):
+        force = kwargs.pop('force', False)
+        if is_master or force:
+            builtin_print(*args, **kwargs)
+
+    __builtin__.print = print
+
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+
+def get_world_size():
+    if not is_dist_avail_and_initialized():
+        return 1
+    return dist.get_world_size()
+
+
+def get_rank():
+    if not is_dist_avail_and_initialized():
+        return 0
+    return dist.get_rank()
+
+
+def is_main_process():
+    return get_rank() == 0
+
+
+def save_on_master(*args, **kwargs):
+    if is_main_process():
+        torch.save(*args, **kwargs)
+
+
+def init_distributed_mode(args):
+    if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
+        args.rank = int(os.environ["RANK"])
+        args.world_size = int(os.environ['WORLD_SIZE'])
+        args.gpu = int(os.environ['LOCAL_RANK'])
+    elif 'SLURM_PROCID' in os.environ:
+        args.rank = int(os.environ['SLURM_PROCID'])
+        args.gpu = args.rank % torch.cuda.device_count()
+    elif hasattr(args, "rank"):
+        pass
+    else:
+        print('Not using distributed mode')
+        args.distributed = False
+        return
+
+    args.distributed = True
+
+    torch.cuda.set_device(args.gpu)
+    args.dist_backend = 'nccl'
+    print('| distributed init (rank {}): {}'.format(
+        args.rank, args.dist_url), flush=True)
+    torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                         world_size=args.world_size, rank=args.rank)
+    setup_for_distributed(args.rank == 0)
+
+def pad_if_smaller(img, size, fill=0):
+    min_size = min(img.size)
+    if min_size < size:
+        ow, oh = img.size
+        padh = size - oh if oh < size else 0
+        padw = size - ow if ow < size else 0
+        img = F.pad(img, (0, 0, padw, padh), fill=fill)
+    return img
+
+def get_dataset(name, image_set, transform):
+    def sbd(*args, **kwargs):
+        return torchvision.datasets.SBDataset(*args, mode='segmentation', **kwargs)
+    paths = {
+        "voc": ('./.data/', torchvision.datasets.VOCSegmentation, 21),
+        "voc_aug": ('/datasets01/SBDD/072318/', sbd, 21),
+    }
+    p, ds_fn, num_classes = paths[name]
+
+    ds = ds_fn(p, image_set=image_set, transforms=transform)
+    return ds, num_classes
+
+
+def get_transform(train):
+    base_size = 520
+    crop_size = 480
+
+    min_size = int((0.5 if train else 1.0) * base_size)
+    max_size = int((2.0 if train else 1.0) * base_size)
+    transforms = []
+    transforms.append(Resize((520, 480)))
+    transforms.append(ToTensor())
+    transforms.append(Normalize(mean=[0.485, 0.456, 0.406],
+                                  std=[0.229, 0.224, 0.225]))
+    return Compose(transforms)
+    
+
+
+def criterion(inputs, target):
+    losses = {}
+    for name, x in inputs.items():
+        losses[name] = nn.functional.cross_entropy(x, target, ignore_index=255)
+
+    if len(losses) == 1:
+        return losses['out']
+
+    return losses['out'] + 0.5 * losses['aux']
+
+
+def evaluate(model, data_loader, device, num_classes):
+    model.eval()
+    confmat = ConfusionMatrix(num_classes)
+    metric_logger = MetricLogger(delimiter="  ")
+    header = 'Test:'
+    with torch.no_grad():
+        for image, target in metric_logger.log_every(data_loader, 100, header):
+            image, target = image.to(device), target.to(device)
+            output = model(image)
+            output = output['out']
+
+            confmat.update(target.flatten(), output.argmax(1).flatten())
+
+        confmat.reduce_from_all_processes()
+
+    return confmat
+
+
+def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, device, epoch, print_freq):
+    model.train()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    for image, target in metric_logger.log_every(data_loader, print_freq, header):
+        image, target = image.to(device), target.to(device)
+        output = model(image)
+        loss = criterion(output, target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        lr_scheduler.step()
+
+        metric_logger.update(loss=loss.item(), lr=optimizer.param_groups[0]["lr"])
+
+
+device = torch.device('cuda')
+dataset_test = torchvision.datasets.VOCSegmentation(root='/content', year='2012', image_set="val", transforms=get_transform(train=False), download=True)
+test_sampler = torch.utils.data.SequentialSampler(dataset_test)
+data_loader_test = torch.utils.data.DataLoader(
+    dataset_test, batch_size=32,
+    sampler=test_sampler, num_workers=4,
+    collate_fn=collate_fn)
+model.to(device)
+confmat = evaluate(model, data_loader_test, device=device, num_classes=21)
+
+confmat.compute()
+print(confmat)
