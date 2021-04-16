a558edad4e2519f6bf4b13532fe21c9cdd9a7d79
rainarit <rraina@ucsd.edu>
2021-04-16 01:07:54 -0700

    new changes

--- core/data/download_coco.py
+++ core/data/download_coco.py
@@ -9,7 +9,7 @@ cur_path = os.path.abspath(os.path.dirname(__file__))
 root_path = os.path.split(os.path.split(os.path.split(cur_path)[0])[0])[0]
 sys.path.append(root_path)
 
-from ..utils import download, makedirs, try_import_pycocotools
+from segmentation_benchmark.core.utils import download, makedirs, try_import_pycocotools
 
 _TARGET_DIR = os.path.expanduser('/coco')
 
