b520d28dd267b7cc402754e3c9e299e8f45d1f67
rainarit <rraina@ucsd.edu>
2021-05-09 04:06:14 -0700

    Update download_coco.py

--- core/data/download_coco.py
+++ core/data/download_coco.py
@@ -7,7 +7,7 @@ import zipfile
 # TODO: optim code
 cur_path = os.path.abspath(os.path.dirname(__file__))
 root_path = os.path.split(os.path.split(os.path.split(cur_path)[0])[0])[0]
-sys.path.append(root_path)
+sys.path.append(root_path) 
 
 from segmentation_benchmark.core.utils import download, makedirs, try_import_pycocotools
 
