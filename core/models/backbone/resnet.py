4b6b5764c4a1dc23d6d684641555ada4b3266c0a
rainarit <rraina@ucsd.edu>
2021-05-09 16:30:46 -0700

    Update resnet.py

--- core/models/backbone/resnet.py
+++ core/models/backbone/resnet.py
@@ -3,7 +3,10 @@ from torch import Tensor
 import torch.nn as nn
 from typing import Type, Any, Callable, Union, List, Optional
 
-from torch.utils.model_zoo import load_url as load_state_dict_from_url
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
@@ -260,7 +263,7 @@ def _resnet(
 ) -> ResNet:
     model = ResNet(block, layers, **kwargs)
     if pretrained:
-        state_dict = load_state_dict_from_url(url=str(model_urls[arch]),
+        state_dict = load_state_dict_from_url(model_urls[arch],
                                               progress=progress)
         model.load_state_dict(state_dict)
     return model
