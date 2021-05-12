7762f16e99217b9c675554b9cbd11b72996eba47
rainarit <rraina@ucsd.edu>
2021-05-12 04:53:35 -0700

    save checkpoint models

--- core/utils/presets.py
+++ core/utils/presets.py
@@ -1,6 +1,5 @@
 from .transforms import *
 
-
 class SegmentationPresetTrain:
     def __init__(self, base_size, crop_size, hflip_prob=0.5, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
         min_size = int(0.5 * base_size)
