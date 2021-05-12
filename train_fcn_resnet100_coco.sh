152bd2f1d4ff8e698fa44a1b50fa2901602cb399
rainarit <rraina@ucsd.edu>
2021-05-12 00:58:38 -0700

    FCN-ResNet100 integration

---
+++ train_fcn_resnet100_coco.sh
@@ -0,0 +1,14 @@
+LEARNING_RATE=0.02
+BATCH_SIZE=4
+WORKERS=4
+BACKBONE='resnet100'
+
+
+echo "Running Model"
+
+python core/scripts/train.py \
+ --lr=${LEARNING_RATE} \
+ --batch-size=${BATCH_SIZE} \
+ --workers=${WORKERS} \
+ --backbone=${BACKBONE} \
+ --aux-loss
