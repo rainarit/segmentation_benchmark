d0dd6ae2ab049fedcd1d32b54d385d79b41cd973
rainarit <rraina@ucsd.edu>
2021-05-13 06:27:55 -0700

    Update train_v1net.py

--- core/scripts/train_v1net.py
+++ core/scripts/train_v1net.py
@@ -60,17 +60,6 @@ def get_transform(train):
     return presets.SegmentationPresetTrain(base_size, crop_size) if train else presets.SegmentationPresetEval(base_size)
 
 
-def criterion(inputs, target):
-    losses = {}
-    for name, x in inputs.items():
-        losses[name] = nn.functional.cross_entropy(x, target, ignore_index=255)
-
-    if len(losses) == 1:
-        return losses['out']
-
-    return losses['out'] + 0.5 * losses['aux']
-
-
 def evaluate(model, data_loader, device, num_classes):
     model.eval()
     confmat = utils.ConfusionMatrix(num_classes)
