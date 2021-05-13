e6b5369f7401c3d0976acb8a83735dbc744fcb4d
rainarit <rraina@ucsd.edu>
2021-05-13 08:04:45 -0700

    Update train_v1net.py

--- core/scripts/train_v1net.py
+++ core/scripts/train_v1net.py
@@ -99,6 +99,32 @@ optimizer = optim.SGD(net.parameters(), lr=1e-2,
                        weight_decay=1e-3)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
+# Training
+def train(epoch):
+  print('\nEpoch: %d' % epoch)
+  net.train()
+  train_loss = 0
+  correct = 0
+  total = 0
+  for batch_idx, (inputs, targets) in enumerate(trainloader):
+    inputs, targets = inputs.to(device), targets.to(device)
+    optimizer.zero_grad()
+    outputs = net(inputs)
+    loss = criterion(outputs, targets)
+    loss.backward()
+    optimizer.step()
+
+    train_loss += loss.item()
+    _, predicted = outputs.max(1)
+    total += targets.size(0)
+    correct += predicted.eq(targets).sum().item()
+    if not batch_idx % 50:
+      print('Iter- %s, Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                    % (batch_idx, 
+                      train_loss/(batch_idx+1), 
+                      100.*correct/total, 
+                      correct, total))
+
 def test(epoch):
   global best_acc
   net.eval()
