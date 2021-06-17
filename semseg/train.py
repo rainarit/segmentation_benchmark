3eaf3a5a7ca3a18d85ab2fe2ee6f9807ce238060
rainarit <rraina@ucsd.edu>
2021-06-16 18:23:40 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -16,7 +16,9 @@ import utils
 import os
 import sys
 
-seed = 3
+seed = 0
+random.seed(seed)     # python random generator
+np.random.seed(seed)  # numpy random generator
 torch.manual_seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
