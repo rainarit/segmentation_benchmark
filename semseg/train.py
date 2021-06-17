d9fe2ec03882959349cc40fd5d669e7a978c11a2
rainarit <rraina@ucsd.edu>
2021-06-17 07:28:40 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -91,7 +91,7 @@ def seed_all(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.cuda.manual_seed(seed)
-    numpy.random.seed(seed)
+    np.random.seed(seed)
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
