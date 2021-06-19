6eacab1b3ccd62b0db51a6850cc2ab5c5846ed67
rainarit <rraina@ucsd.edu>
2021-06-19 09:50:33 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -130,8 +130,7 @@ def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, devi
         colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
         colors = (colors % 255).numpy().astype("uint8")
         # plot the semantic segmentation predictions of 21 classes in each color
-        print(tuple(input_image.size()))
-        r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(input_image)
+        r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(tuple(input_image.size()))
         r.putpalette(colors)
 
 
