cb113eb7b151549097ad4f0e9fb5c0cfa1609f6b
rainarit <rraina@ucsd.edu>
2021-06-19 10:24:30 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -132,7 +132,8 @@ def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, devi
 
 
 
-        writer.add_image('train_image', output['out'][0].argmax(0), dataformats='HW')
+        #writer.add_image('train_image', output['out'][0].argmax(0), dataformats='HW')
+        writer.add_image('train_image', np.array(r), dataformats='HW')
 
         step = step + 1
         writer.flush()
