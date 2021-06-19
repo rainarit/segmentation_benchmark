4413ca7368ce252ad32736fff0ee4667f6ec06a4
rainarit <rraina@ucsd.edu>
2021-06-19 09:52:27 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -93,9 +93,6 @@ def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, devi
     metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value}'))
     header = 'Epoch: [{}]'.format(epoch)
     for image, target in metric_logger.log_every(data_loader, print_freq, header):
-
-        input_image = image
-
         image, target = image.to(device), target.to(device)
         output = model(image)
         #torch.set_deterministic(False)
@@ -130,7 +127,7 @@ def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, devi
         colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
         colors = (colors % 255).numpy().astype("uint8")
         # plot the semantic segmentation predictions of 21 classes in each color
-        r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(tuple(input_image.size()))
+        r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize((480, 540))
         r.putpalette(colors)
 
 
