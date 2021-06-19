dd89b242d060a106caa147b651052d854387f4a6
rainarit <rraina@ucsd.edu>
2021-06-19 11:15:30 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -120,10 +120,9 @@ def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, devi
 
         writer.add_scalar("Mean IoU/train", confmat_train_iu.mean().item() * 100, step)
         writer.add_scalar("Pixel Accuracy/train", confmat_train_acc_global.item() * 100, step)
-
-        print(image.shape)
         
         output_predictions = output['out'][0].argmax(0)
+        print(output_predictions.shape)
         # create a color pallette, selecting a color for each class
         palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
         colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
