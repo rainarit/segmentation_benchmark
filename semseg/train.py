ec16b4944386aae7e77caaf1a13f231a59baf2fb
rainarit <rraina@ucsd.edu>
2021-06-24 13:52:01 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -112,6 +112,8 @@ def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, devi
     metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value}'))
     header = 'Epoch: [{}]'.format(epoch)
     for image, target in metric_logger.log_every(data_loader, print_freq, header):
+        print(image.shape)
+        print(target.shape)
         img = np.reshape(image[0], (-1, 480, 480, 3))
         tgt = np.reshape(target[0], (-1, 480, 480, 1))
         writer.add_image('Images/train_original', img, train_step, dataformats='NHWC')
