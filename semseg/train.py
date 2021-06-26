a79e2f9d3ec9b8bc0166149232e9a05da5bf8031
rainarit <rraina@ucsd.edu>
2021-06-25 20:06:20 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -1,7 +1,6 @@
 import datetime
 import os
 import time
-
 import torch
 import torch.utils.data
 from torch import nn
@@ -9,69 +8,28 @@ import torchvision
 import numpy as np
 import random
 from PIL import Image
-
-from coco_seg import COCOSegmentation
-from torch.utils.data.sampler import Sampler, BatchSampler
-import torch.utils.data as data
-
 from coco_utils import get_coco
 import presets
 import utils
-
 import os
 import sys
-
 import torch
-
-from torchvision import transforms
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter()
-
 train_step = 0
 val_step = 0
 running_loss = 0.0
-
 seed=42
 random.seed(seed)
 os.environ['PYTHONHASHSEED'] = str(seed)
 np.random.seed(seed)
-
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
-
 g = torch.Generator()
 g.manual_seed(42)
-
-class IterationBasedBatchSampler(BatchSampler):
-    """
-    Wraps a BatchSampler, resampling from it until
-    a specified number of iterations have been sampled
-    """
-
-    def __init__(self, batch_sampler, num_iterations, start_iter=0):
-        self.batch_sampler = batch_sampler
-        self.num_iterations = num_iterations
-        self.start_iter = start_iter
-
-    def __iter__(self):
-        iteration = self.start_iter
-        while iteration <= self.num_iterations:
-            # if the underlying sampler has a set_epoch method, like
-            # DistributedSampler, used for making each process see
-            # a different split of the dataset, then set it
-            if hasattr(self.batch_sampler.sampler, "set_epoch"):
-                self.batch_sampler.sampler.set_epoch(iteration)
-            for batch in self.batch_sampler:
-                iteration += 1
-                if iteration > self.num_iterations:
-                    break
-                yield batch
-
-    def __len__(self):
-        return self.num_iterations
     
 def get_dataset(dir_path, name, image_set, transform):
     def sbd(*args, **kwargs):
@@ -80,10 +38,8 @@ def get_dataset(dir_path, name, image_set, transform):
         "coco": (dir_path, get_coco, 21)
     }
     p, ds_fn, num_classes = paths[name]
-
     ds = ds_fn(p, image_set=image_set, transforms=transform)
     return ds, num_classes
-
 
 def get_mask(output):
     output_predictions = output['out'][0].argmax(0)
@@ -96,27 +52,21 @@ def get_mask(output):
     r.putpalette(colors)
     return np.array(r.convert('RGB'))
 
-
 def get_transform(train):
     base_size = 520
     crop_size = 480
-
     return presets.SegmentationPresetTrain(base_size, crop_size) if train else presets.SegmentationPresetEval(base_size)
-
 
 def criterion(inputs, target):
     losses = {}
     for name, x in inputs.items():
         losses[name] = nn.functional.cross_entropy(x, target, ignore_index=255)
-
     if len(losses) == 1:
         return losses['out']
-
     return losses['out'] + 0.5 * losses['aux']
-
+    
 def evaluate(model, data_loader, device, num_classes):
     global val_step
-
     model.eval()
     confmat = utils.ConfusionMatrix(num_classes)
     metric_logger = utils.MetricLogger(delimiter="  ")
@@ -126,139 +76,73 @@ def evaluate(model, data_loader, device, num_classes):
             image, target = image.to(device), target.to(device)
             output = model(image)
             writer.add_image('Images/val', get_mask(output), val_step, dataformats='HWC')
-
             output = output['out']
-
             val_step = val_step + 1
-
             confmat.update(target.flatten(), output.argmax(1).flatten())
-
         confmat.reduce_from_all_processes()
-
     return confmat
-
 
 def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, device, epoch, print_freq):
     global train_step
-
     model.train()
     metric_logger = utils.MetricLogger(delimiter="  ")
     metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value}'))
     header = 'Epoch: [{}]'.format(epoch)
     for image, target in metric_logger.log_every(data_loader, print_freq, header):
         writer.add_image('Images/train_original', image, train_step, dataformats='NCHW')
-
         image, target = image.to(device), target.to(device)
-
         output = model(image)
-
         #torch.set_deterministic(False)
         loss = criterion(output, target)
         #torch.set_deterministic(True)
-
         optimizer.zero_grad()
-
         #torch.set_deterministic(False)
         loss.backward()
         #torch.set_deterministic(True)
         
         optimizer.step()
-
         lr_scheduler.step()
-
         metric_logger.update(loss=loss.item(), lr=optimizer.param_groups[0]["lr"])
-
         writer.add_scalar("Loss/train", loss.item(), train_step)
         writer.add_scalar("Learning Rate", optimizer.param_groups[0]["lr"], train_step)
-
         confmat_train = utils.ConfusionMatrix(21)
         confmat_train.update(target.flatten(), output['out'].argmax(1).flatten())
         confmat_train_acc_global, confmat_train_acc, confmat_train_iu = confmat_train.compute()
-
         writer.add_scalar("Mean IoU/train", confmat_train_iu.mean().item() * 100, train_step)
         writer.add_scalar("Pixel Accuracy/train", confmat_train_acc_global.item() * 100, train_step)
         writer.add_image('Images/train_prediction', get_mask(output), train_step, dataformats='HWC')
-
         train_step = train_step + 1
         writer.flush()
-
     confmat_train.reduce_from_all_processes()
-
 
 def seed_worker(worker_id):
     worker_seed = torch.initial_seed() % 2**32
     np.random.seed(worker_seed)
     random.seed(worker_seed)
 
-def make_batch_data_sampler(sampler, images_per_batch, num_iters=None, start_iter=0):
-    batch_sampler = data.sampler.BatchSampler(sampler, images_per_batch, drop_last=True)
-    if num_iters is not None:
-        batch_sampler = IterationBasedBatchSampler(batch_sampler, num_iters, start_iter)
-    return batch_sampler
-
 def main(args):
-
     if args.output_dir:
         utils.mkdir(args.output_dir)
-
     utils.init_distributed_mode(args)
-
     print(args)
-
     device = torch.device(args.device)
-
-    # image transform
-    input_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize([.485, .456, .406], [.229, .224, .225]),
-    ])
-    # dataset and dataloader
-    data_kwargs = {'transform': input_transform, 'base_size': 520, 'crop_size': 480}
-
-    dataset = COCOSegmentation(split='train', mode='train', **data_kwargs)
-    dataset_test = COCOSegmentation(split='val', mode='val', **data_kwargs)
-    num_classes = 21
-
-    args.iters_per_epoch = len(dataset) // (args.num_gpus * args.batch_size)
-    args.max_iters = args.epochs * args.iters_per_epoch
-
-    #dataset, num_classes = get_dataset(args.data_path, args.dataset, "train", get_transform(train=True))
-    #dataset_test, _ = get_dataset(args.data_path, args.dataset, "val", get_transform(train=False))
-
+    dataset, num_classes = get_dataset(args.data_path, args.dataset, "train", get_transform(train=True))
+    dataset_test, _ = get_dataset(args.data_path, args.dataset, "val", get_transform(train=False))
     train_sampler = torch.utils.data.RandomSampler(dataset)
     test_sampler = torch.utils.data.SequentialSampler(dataset_test)
-
-    
-    train_batch_sampler = make_batch_data_sampler(train_sampler, args.batch_size, args.max_iters)
-    val_batch_sampler = make_batch_data_sampler(test_sampler, args.batch_size)
-
-
-    #data_loader = torch.utils.data.DataLoader(
-    #    dataset, batch_size=args.batch_size,
-    #    sampler=train_batch_sampler, num_workers=args.workers,
-    #    collate_fn=utils.collate_fn, pin_memory=True)
-
-    #data_loader_test = torch.utils.data.DataLoader(
-    #    dataset_test, batch_size=1,
-    #    sampler=val_batch_sampler, num_workers=args.workers,
-    #    collate_fn=utils.collate_fn, pin_memory=True)
-
-    data_loader = data.DataLoader(dataset=dataset,
-                                            batch_sampler=train_batch_sampler,
-                                            num_workers=args.workers,
-                                            pin_memory=True)
-    data_loader_test = data.DataLoader(dataset=dataset_test,
-                                          batch_sampler=val_batch_sampler,
-                                          num_workers=args.workers,
-                                          pin_memory=True)
-
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=args.batch_size,
+        sampler=train_sampler, num_workers=args.workers,
+        collate_fn=utils.collate_fn, drop_last=True)
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1,
+        sampler=test_sampler, num_workers=args.workers,
+        collate_fn=utils.collate_fn)
     model = torchvision.models.segmentation.__dict__[args.model](num_classes=num_classes,
                                                                  aux_loss=args.aux_loss,
                                                                  pretrained=args.pretrained)
     model.to(device)
-
     model_without_ddp = model
-
     params_to_optimize = [
         {"params": [p for p in model_without_ddp.backbone.parameters() if p.requires_grad]},
         {"params": [p for p in model_without_ddp.classifier.parameters() if p.requires_grad]},
@@ -269,45 +153,38 @@ def main(args):
     optimizer = torch.optim.SGD(
         params_to_optimize,
         lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
-
     lr_scheduler = torch.optim.lr_scheduler.LambdaLR(
         optimizer,
         lambda x: (1 - x / (len(data_loader) * args.epochs)) ** 0.9)
-
     if args.test_only:
         confmat = evaluate(model, data_loader_test, device=device, num_classes=num_classes)
         print(confmat)
         return
-
     start_time = time.time()
     for epoch in range(args.start_epoch, args.epochs):
         train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, device, epoch, args.print_freq)
         confmat = evaluate(model, data_loader_test, device=device, num_classes=num_classes)
         print(confmat)
-
         writer.add_scalar("Mean IoU/val", confmat.iu, epoch)
         writer.add_scalar("Pixel Accuracy/val", confmat.acc_global, epoch)
         writer.flush()
-
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
 
-
 def get_args_parser(add_help=True):
-    import argparse
-    parser = argparse.ArgumentParser(description='PyTorch Segmentation Training', add_help=add_help)
+     import argparse
+     parser = argparse.ArgumentParser(description='PyTorch Segmentation Training', add_help=add_help)
 
-    parser.add_argument('--data-path', default='/home/AD/rraina/segmentation_benchmark/coco', help='dataset path')
-    parser.add_argument('--dataset', default='coco', help='dataset name')
-    parser.add_argument('--model', default='fcn_resnet101', help='model')
-    parser.add_argument('--num-gpus', default=1, type=int)
-    parser.add_argument('--aux-loss', action='store_true', help='auxiliar loss')
+     parser.add_argument('--data-path', default='/home/AD/rraina/segmentation_benchmark/coco/images', help='dataset path')
+     parser.add_argument('--data-path', default='/home/AD/rraina/segmentation_benchmark/coco', help='dataset path')
+     parser.add_argument('--dataset', default='coco', help='dataset name')
+     parser.add_argument('--model', default='fcn_resnet101', help='model')
+     parser.add_argument('--aux-loss', action='store_true', help='auxiliar loss')
     parser.add_argument('--device', default='cuda', help='device')
     parser.add_argument('-b', '--batch-size', default=8, type=int)
     parser.add_argument('--epochs', default=30, type=int, metavar='N',
                         help='number of total epochs to run')
-
     parser.add_argument('-j', '--workers', default=16, type=int, metavar='N',
                         help='number of data loading workers (default: 16)')
     parser.add_argument('--lr', default=0.01, type=float, help='initial learning rate')
@@ -333,7 +210,6 @@ def get_args_parser(add_help=True):
         help="Use pre-trained models from the modelzoo",
         action="store_true",
     )
-
     return parser
 
 if __name__ == "__main__":
