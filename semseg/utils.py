bdb18df7de01b311d9e3f8625b1d01571277cc3e
rainarit <rraina@ucsd.edu>
2021-06-29 10:53:59 -0700

    Update utils.py

--- semseg/utils.py
+++ semseg/utils.py
@@ -248,6 +248,7 @@ def setup_for_distributed(is_master):
             builtin_print(*args, **kwargs)
 
     __builtin__.print = print
+    print("Built distributed mode")
 
 
 def is_dist_avail_and_initialized():
