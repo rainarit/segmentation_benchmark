49d3000ba722edfb5d339fc978c090e4e6baf246
rainarit <rraina@ucsd.edu>
2021-07-13 02:50:22 -0700

    Update deeplabv3.py

--- semseg/deeplabv3.py
+++ semseg/deeplabv3.py
@@ -16,8 +16,6 @@ import sys
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-from tqdm import tqdm
-
 writer = SummaryWriter()
 
 seed=42
@@ -34,8 +32,120 @@ g.manual_seed(42)
 
 evaluate_step = 0
 train_step = 0
+    
+def get_dataset(dir_path, name, image_set, transform):
+    def sbd(*args, **kwargs):
+        return torchvision.datasets.SBDataset(*args, mode='segmentation', **kwargs)
+    paths = {
+        "coco": (dir_path, get_coco, 21), 
+        "voc": (dir_path, torchvision.datasets.VOCSegmentation, 21),
+    }
+    p, ds_fn, num_classes = paths[name]
+    if name == "voc":
+        ds = ds_fn(p, image_set=image_set, transforms=transform, download=True)
+    else:
+        ds = ds_fn(p, image_set=image_set, transforms=transform)
+    return ds, num_classes
 
-def train():
+def get_mask(output):
+    output_predictions = output['out'][0].argmax(0)
+    # create a color pallette, selecting a color for each class
+    palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
+    colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
+    colors = (colors % 255).numpy().astype("uint8")
+    # plot the semantic segmentation predictions of 21 classes in each color
+    r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize((480,480))
+    r.putpalette(colors)
+    return np.array(r.convert('RGB'))
+
+def get_transform(train):
+    base_size = 520
+    crop_size = 480
+    return presets.SegmentationPresetTrain(base_size, crop_size) if train else presets.SegmentationPresetEval(base_size)
+
+def criterion(inputs, target):
+    losses = {}
+    for name, x in inputs.items():
+        losses[name] = nn.functional.cross_entropy(x, target, ignore_index=255)
+    if len(losses) == 1:
+        return losses['out']
+    return losses['out'] + 0.5 * losses['aux']
+
+def evaluate(model, data_loader, device, num_classes):
+
+    global evaluate_step
+
+    model.eval()
+    confmat = utils.ConfusionMatrix(num_classes)
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Test:'
+    with torch.no_grad():
+        for image, target in metric_logger.log_every(data_loader, 100, header):
+            image, target = image.to(device), target.to(device)
+
+            output = model(image)
+
+            writer.add_image('Images/val', get_mask(output), evaluate_step, dataformats='HWC')
+            
+            output = output['out']
+            confmat.update(target.flatten(), output.argmax(1).flatten())
+
+            writer.flush()
+
+            evaluate_step+=1
+
+            
+
+        confmat.reduce_from_all_processes()
+    return confmat
+
+def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, device, epoch, print_freq):
+    global train_step
+
+    model.train()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value}'))
+    header = 'Epoch: [{}]'.format(epoch)
+
+    for image, target in metric_logger.log_every(data_loader, print_freq, header):
+        writer.add_image('Images/train_original', image, train_step, dataformats='NCHW')
+
+        image, target = image.to(device), target.to(device)
+
+        output = model(image)
+
+        loss = criterion(output, target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        lr_scheduler.step()
+
+        metric_logger.update(loss=loss.item(), lr=optimizer.param_groups[0]["lr"])
+
+        writer.add_scalar("Loss/train", loss.item(), train_step)
+        writer.add_scalar("Learning Rate", optimizer.param_groups[0]["lr"], train_step)
+
+        confmat_train = utils.ConfusionMatrix(21)
+        confmat_train.update(target.flatten(), output['out'].argmax(1).flatten())
+        confmat_train_acc_global, confmat_train_acc, confmat_train_iu = confmat_train.compute()
+
+        writer.add_scalar("Mean IoU/train", confmat_train_iu.mean().item() * 100, train_step)
+        writer.add_scalar("Pixel Accuracy/train", confmat_train_acc_global.item() * 100, train_step)
+        writer.add_image('Images/train_prediction', get_mask(output), train_step, dataformats='HWC')
+        writer.flush()
+
+        train_step += 1
+
+    confmat_train.reduce_from_all_processes()
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+def main(args):
     if args.output_dir:
         utils.mkdir(args.output_dir)
 
@@ -46,20 +156,28 @@ def train():
     device = torch.device(args.device)
 
     dataset, num_classes = get_dataset(args.data_path, args.dataset, "train", get_transform(train=True))
+    dataset_test, _ = get_dataset(args.data_path, args.dataset, "val", get_transform(train=False))
 
     if args.distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+        test_sampler = torch.utils.data.distributed.DistributedSampler(dataset_test)
     else:
         train_sampler = torch.utils.data.RandomSampler(dataset)
-    
+        test_sampler = torch.utils.data.SequentialSampler(dataset_test)
+
     data_loader = torch.utils.data.DataLoader(
         dataset, batch_size=args.batch_size,
         sampler=train_sampler, num_workers=args.workers,
         collate_fn=utils.collate_fn, drop_last=True)
 
-    model = torchvision.models.segmentation.__dict__[args.model](num_classes=num_classes,
-                                                                 aux_loss=args.aux_loss,
-                                                                 pretrained=args.pretrained)
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1,
+        sampler=test_sampler, num_workers=args.workers,
+        collate_fn=utils.collate_fn)
+
+
+    model = torch.hub.load('github', 'deeplabv3_resnet101', pretrained=False)
+
     model.to(device)
 
     if args.distributed:
@@ -87,18 +205,12 @@ def train():
     lr_scheduler = torch.optim.lr_scheduler.LambdaLR(
         optimizer,
         lambda x: (1 - x / (len(data_loader) * args.epochs)) ** 0.9)
-
-    # Path to save models
-    checkpoint_dir = os.path.join(
-        args.output_dir,
-        "models",
-        "voc12",
-        str(args.model).lower(),
-        "train",
-    )
-    utils.mkdir(checkpoint_dir)
-    print("Checkpoint dst:", checkpoint_dir)
-
+    
+    if args.test_only:
+        confmat = evaluate(model, data_loader_test, device=device, num_classes=num_classes)
+        print(confmat)
+        return
+    
     start_time = time.time()
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
@@ -121,18 +233,16 @@ def train():
             'epoch': epoch,
             'args': args
         }
-
         utils.save_on_master(
             checkpoint,
-            os.path.join(checkpoint_dir, 'model_{}.pth'.format(epoch)))
+            os.path.join(args.output_dir, 'model_{}.pth'.format(epoch)))
         utils.save_on_master(
             checkpoint,
-            os.path.join(checkpoint_dir, 'checkpoint.pth'))
+            os.path.join(args.output_dir, 'checkpoint.pth'))
     
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
-
 
 def get_args_parser(add_help=True):
     import argparse
@@ -155,7 +265,7 @@ def get_args_parser(add_help=True):
                         metavar='W', help='weight decay (default: 1e-4)',
                         dest='weight_decay')
     parser.add_argument('--print-freq', default=10, type=int, help='print frequency')
-    parser.add_argument('--output-dir', default='/home/AD/rraina/segmentation_benchmark/semseg/output_dir', help='path where to save')
+    parser.add_argument('--output-dir', default='.', help='path where to save')
     parser.add_argument('--resume', default='', help='resume from checkpoint')
     parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                         help='start epoch')
