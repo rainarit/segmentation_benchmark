22eb78963f55d1499c56fc0682024441428b8a41
rainarit <rraina@ucsd.edu>
2021-07-14 13:54:26 -0700

    Update eval.py

--- semseg/eval.py
+++ semseg/eval.py
@@ -146,7 +146,7 @@ def main(args):
             logits = logits['out']
 
             # Save on disk for CRF post-processing
-            filename = os.path.join(logit_dir, image_id + ".npy")
+            filename = os.path.join(str(logit_dir), str(image_id) + ".npy")
             np.save(filename, logits.cpu().numpy())
 
             confmat.update(target.flatten(), logits.argmax(1).flatten())
