b37905bed2afbc4d4485abdfa6e1c1a8a4243292
rainarit <rraina@ucsd.edu>
2021-07-15 01:19:43 -0700

    Update crf.py

--- semseg/crf.py
+++ semseg/crf.py
@@ -135,10 +135,11 @@ def main(args):
     # Process per sample
     def process(i):
         image, target = dataset_test.__getitem__(i)
-        #image, target = image.to(device), target.to(device)
 
         filename = os.path.join(str(logit_dir), str(i) + ".npy")
         logit = torch.from_numpy(np.load(filename))
+
+        
         print(logit.shape)
 
         prob = postprocessor(image, logit)
