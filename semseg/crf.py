da19e0d6b73005614da89c17e79d075a8a53e63b
rainarit <rraina@ucsd.edu>
2021-07-15 10:58:05 -0700

    Update crf.py

--- semseg/crf.py
+++ semseg/crf.py
@@ -139,6 +139,7 @@ def main(args):
         filename = os.path.join(str(logit_dir), str(i) + ".npy")
         logit = torch.from_numpy(np.load(filename))
 
+        print(logit.shape)
         
         prob = postprocessor(image, logit)
 
