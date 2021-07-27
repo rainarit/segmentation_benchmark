3a69fd26db19c42207d7a81c7634d5f6b11b8096
rainarit <rraina@ucsd.edu>
2021-07-26 19:50:59 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -84,11 +84,10 @@ def evaluate(model, data_loader, device, num_classes, iterator):
             print(image[0].shape)
             print(target.shape)
             writer.add_image('Images/val_image', image[0], iterator.eval_step, dataformats='CHW')
-            writer.add_image('Images/val_target', target, iterator.eval_step, dataformats='NHW')
             output = model(image)
 
             writer.add_image('Images/val_output', get_mask(output), iterator.eval_step, dataformats='HWC')
-            
+            print(output['out'][0].argmax(0).shape)
             output = output['out']
             confmat.update(target.flatten(), output.argmax(1).flatten())
 
