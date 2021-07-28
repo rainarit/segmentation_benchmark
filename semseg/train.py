fc7de2ee96d2e35ae370153169af8c1da8a485b7
rainarit <rraina@ucsd.edu>
2021-07-28 12:10:48 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -29,7 +29,7 @@ from torch.utils.tensorboard import SummaryWriter
 seed=42
 random.seed(seed)
 os.environ['PYTHONHASHSEED'] = str(seed)
-os.environ['CUDA_VISIBLE_DEVICES']="0, 1, 2, 3"
+#os.environ['CUDA_VISIBLE_DEVICES']="0, 1, 2, 3"
 np.random.seed(seed)
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
