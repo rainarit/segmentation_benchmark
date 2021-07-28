1b7208a2abd15c8ca1205d9340492084f1d76bd5
rainarit <rraina@ucsd.edu>
2021-07-28 11:39:17 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -101,7 +101,7 @@ def evaluate(model, data_loader, device, num_classes, iterator):
 
     with torch.no_grad():
         for idx, (image, target) in enumerate(metric_logger.log_every(data_loader, 4, header)):
-            print(os.environ["CUDA_VISIBLE_DEVICES"])
+            print(args.gpu_id)
             image, target = image.to(device), target.to(device)
             output = model(image)
             output = output['out']
