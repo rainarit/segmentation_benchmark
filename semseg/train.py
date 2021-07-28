83728b09d8666eb06a21821180c2bdac9f9e1a8a
rainarit <rraina@ucsd.edu>
2021-07-28 13:21:12 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -284,7 +284,6 @@ def get_args_parser(add_help=True):
 
     parser.add_argument('--data-path', default='/home/AD/rraina/segmentation_benchmark/', help='dataset path')
     parser.add_argument('--dataset', default='coco', help='dataset name')
-    parser.add_argument("--rank", type=int)
     parser.add_argument('--model', default='fcn_resnet101', help='model')
     parser.add_argument('--aux-loss', action='store_true', help='auxiliar loss')
     parser.add_argument('--device', default='cuda', help='device')
