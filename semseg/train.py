6dec31b9794899060c0fd2792c5936b5c8ce80cf
rainarit <rraina@ucsd.edu>
2021-07-28 13:20:55 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -182,7 +182,7 @@ def main(args):
     print(args)
 
     iterator = utils.Iterator()
-    rank = args.local_rank
+    rank = args.rank
 
     device = torch.device(f'cuda:{rank}')
 
@@ -284,7 +284,7 @@ def get_args_parser(add_help=True):
 
     parser.add_argument('--data-path', default='/home/AD/rraina/segmentation_benchmark/', help='dataset path')
     parser.add_argument('--dataset', default='coco', help='dataset name')
-    parser.add_argument("--local_rank", type=int)
+    parser.add_argument("--rank", type=int)
     parser.add_argument('--model', default='fcn_resnet101', help='model')
     parser.add_argument('--aux-loss', action='store_true', help='auxiliar loss')
     parser.add_argument('--device', default='cuda', help='device')
