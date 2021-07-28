c1137e9fdf7d7bfb01d1329642686b12e7f886e5
rainarit <rraina@ucsd.edu>
2021-07-27 21:53:52 -0700

    Update train.py

--- semseg/train.py
+++ semseg/train.py
@@ -93,7 +93,6 @@ def evaluate(model, data_loader, device, num_classes, iterator):
 
             writer.flush()
             iterator.add_eval()
-            break
 
         confmat.reduce_from_all_processes()
     return confmat
