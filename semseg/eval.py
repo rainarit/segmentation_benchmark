0e8f413c7bae31d4ba733282a41fc893181bbc46
rainarit <rraina@ucsd.edu>
2021-07-28 17:10:14 -0700

    Update eval.py

--- semseg/eval.py
+++ semseg/eval.py
@@ -237,7 +237,7 @@ def main(args):
             images.save(str(filename))
 
             # Saving Processed Image
-            processed_image = Image.fromarray(image.numpy()[0])
+            processed_image = Image.fromarray(image.cpu().numpy()[0])
             filename = os.path.join(processed_image_dir, str(idx) + ".png")
             processed_image.save(str(processed_image))
 
