c3bddcda3034750fb2a772f9e29bd8b59073cd66
rainarit <rraina@ucsd.edu>
2021-07-29 02:27:23 -0700

    Update crf.py

--- semseg/crf.py
+++ semseg/crf.py
@@ -171,7 +171,7 @@ def main(args):
         [joblib.delayed(process)(i) for i in range(1)]
     )
 
-    print(len(results))
+    print(results[0])
     
     for i, (preds, gts) in enumerate(*results):   
         confmat.update(gts.flatten(), preds.argmax(0).flatten())
