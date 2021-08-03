def48f9e8c635f673897b1a5ec10a0e0099956d8
rainarit <rraina@ucsd.edu>
2021-08-02 21:11:17 -0700

    Update resnet_v1net.py

--- semseg/models/resnet_v1net.py
+++ semseg/models/resnet_v1net.py
@@ -234,7 +234,6 @@ class ResNet(nn.Module):
         # See note [TorchScript super()]
         x = self.conv1(x)
         x = self.div(x)['out']
-        print("HELLOO!!")
 
         if use_bn1:
             x = self.bn1(x)
@@ -314,6 +313,7 @@ def resnet101(pretrained: bool = False, progress: bool = True, **kwargs: Any) ->
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
+    print("YOOYO")
     return _resnet('resnet101', Bottleneck, [3, 4, 23, 3], pretrained, progress,
                    **kwargs)
 
