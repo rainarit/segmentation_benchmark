e20da18cebe278063a40c65a9e187f57ac0ec97e
rainarit <rraina@ucsd.edu>
2021-08-04 20:20:41 -0700

    Update _utils.py

--- semseg/models/segmentation/_utils.py
+++ semseg/models/segmentation/_utils.py
@@ -14,9 +14,9 @@ class _SimpleSegmentationModel(nn.Module):
         self.aux_classifier = aux_classifier
 
     def forward(self, x):
-        print("In Simple Seg Models")
         input_shape = x.shape[-2:]
         # contract: features is a dict of tensors
+        print(self.backbone)
         features = self.backbone(x)
 
         result = OrderedDict()
