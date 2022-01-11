482189db3474500cd64bbb0b0810d9e776fb627b
Vijay <vijayvee@gmail.com>
2022-01-11 20:40:42 +0000

    Updated DivNormEI and DivNorm (e/i lesion)

--- semseg/models/divisive_norm_exc_inh.py
+++ semseg/models/divisive_norm_exc_inh.py
@@ -1,4 +1,5 @@
 """Schwartz and Simoncelli 2001 + excitation and inhibition, in pytorch."""
+from numpy.core.numeric import True_
 import torch  # pylint: disable=import-error
 import torch.nn as nn  # pylint: disable=import-error
 import torch.nn.functional as F  # pylint: disable=import-error
@@ -53,7 +54,6 @@ def generate_gabor_filter_weights(sz, l_theta, l_sfs,
         return gabor_bank, theta2filter
     return gabor_bank
 
-
 # define normalized 2D gaussian
 def get_gaussian_filterbank(n_filters, f_sz, device='cuda'):
     """Generate a torch tensor for conv2D weights resembling 2D gaussian"""
@@ -78,7 +78,6 @@ def get_gaussian_filterbank(n_filters, f_sz, device='cuda'):
     filters = filters.reshape((n_filters, 1, f_sz, f_sz))
     filters = torch.Tensor(filters).float().to(device)
     return filters
-
 
 def nonnegative_weights_init(m):
     """Non-negative initialization of weights."""
@@ -179,6 +178,7 @@ class DivNormExcInh(nn.Module):
                  groups=1,
                  device='cuda',
                  alexnet_lrn=False,
+                 gaussian_init=True,
                  ):
         super(DivNormExcInh, self).__init__()
         self.in_channels = in_channels
@@ -199,9 +199,10 @@ class DivNormExcInh(nn.Module):
                 divnorm_fsize,
                 padding=(divnorm_fsize - 1) // 2,
                 padding_mode=padding_mode,
-                groups=self.hidden_dim,
+                groups=groups,
                 bias=False)
-            gaussian_weights_init(self.div)
+            if gaussian_init:
+                gaussian_weights_init(self.div)
             self.e_e = nn.Conv2d(
                 self.hidden_dim, self.hidden_dim, 
                 exc_fsize, bias=True, padding=(exc_fsize - 1) // 2,
@@ -214,7 +215,7 @@ class DivNormExcInh(nn.Module):
             self.output_bn = nn.BatchNorm2d(in_channels)
             self.output_relu = nn.ReLU(inplace=True)
     
-    def forward(self, x, residual=True, square_act=True):
+    def forward(self, x, residual=True, square_act=True, hor_conn=True):
         """
         params:
             x: Input activation tensor
@@ -225,7 +226,7 @@ class DivNormExcInh(nn.Module):
             del residual, square_act
             return self.forward_alexnet_lrn(x)
         else:
-            return self.forward_divnormei(x, residual, square_act)
+            return self.forward_divnormei(x=x, residual=residual, square_act=square_act, hor_conn=hor_conn)
         
     def forward_divnormei(self, x, residual=True, square_act=True, hor_conn=True):
         """
@@ -243,27 +244,28 @@ class DivNormExcInh(nn.Module):
             simple_cells = nn.Identity()(x)
 
         if square_act:
-            # TODO(vveeraba): vijay removed squaring for NaN debugging
-            # simple_cells = simple_cells ** 2
-            # denominator to be a function of the numerator (fraction of numerator)
-            # Make sure that the self connection is some small non-zero value
+            simple_cells = simple_cells ** 2
             norm = self.div(simple_cells) + self.sigma ** 2 + 1e-5
             if (norm == 0).any():
                 import ipdb; ipdb.set_trace()
-            simple_cells = torch.div(simple_cells, norm)
+            simple_cells = simple_cells / norm
         else:
             norm = 1 + F.relu(self.div(simple_cells))
+            #norm = F.relu(self.div(simple_cells)) + self.sigma ** 2 + 1e-5
             simple_cells = simple_cells / norm
 
         if hor_conn:
             inhibition = self.i_e(simple_cells)  # + self.i_ff(x)
             # Excitatory lateral connections (Center corresponds to self-excitation)
             excitation = self.e_e(simple_cells)
+            #output = simple_cells + excitation - inhibition
             output = self.output_bn(simple_cells + excitation - inhibition)
         else:
             output = self.output_bn(simple_cells)
+
         if residual:
             output += identity
+
         output = self.output_relu(output)
         return output
 
@@ -277,3 +279,142 @@ class DivNormExcInh(nn.Module):
             as described in https://proceedings.neurips.cc/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
         """
         pass
+
+class GaborFilters(nn.Module):
+    def __init__(self, 
+        in_channels, 
+        n_sigmas = 3,
+        n_lambdas = 4,
+        n_gammas = 1,
+        n_thetas = 7,
+        kernel_radius=15,
+        rotation_invariant=True
+    ):
+        super().__init__()
+        self.in_channels = in_channels
+        kernel_size = kernel_radius*2 + 1
+        self.kernel_size = kernel_size
+        self.kernel_radius = kernel_radius
+        self.n_thetas = n_thetas
+        self.rotation_invariant = rotation_invariant
+        def make_param(in_channels, values, requires_grad=True, dtype=None):
+            if dtype is None:
+                dtype = 'float32'
+            values = np.require(values, dtype=dtype)
+            n = in_channels * len(values)
+            data=torch.from_numpy(values).view(1,-1)
+            data = data.repeat(in_channels, 1)
+            return torch.nn.Parameter(data=data, requires_grad=requires_grad)
+
+
+        # build all learnable parameters
+        self.sigmas = make_param(in_channels, 2**np.arange(n_sigmas)*2)
+        self.lambdas = make_param(in_channels, 2**np.arange(n_lambdas)*4.0)
+        self.gammas = make_param(in_channels, np.ones(n_gammas)*0.5)
+        self.psis = make_param(in_channels, np.array([0, math.pi/2.0]))
+
+        print(len(self.sigmas))
+
+
+        thetas = np.linspace(0.0, 2.0*math.pi, num=n_thetas, endpoint=False)
+        thetas = torch.from_numpy(thetas).float()
+        self.register_buffer('thetas', thetas)
+
+        indices = torch.arange(kernel_size, dtype=torch.float32) -  (kernel_size - 1)/2
+        self.register_buffer('indices', indices)
+
+
+        # number of channels after the conv
+        self._n_channels_post_conv = self.in_channels * self.sigmas.shape[1] * \
+                                     self.lambdas.shape[1] * self.gammas.shape[1] * \
+                                     self.psis.shape[1] * self.thetas.shape[0] 
+
+
+    def make_gabor_filters(self):
+
+        sigmas=self.sigmas
+        lambdas=self.lambdas
+        gammas=self.gammas
+        psis=self.psis
+        thetas=self.thetas
+        y=self.indices
+        x=self.indices
+
+        in_channels = sigmas.shape[0]
+        assert in_channels == lambdas.shape[0]
+        assert in_channels == gammas.shape[0]
+
+        kernel_size = y.shape[0], x.shape[0]
+
+
+
+        sigmas  = sigmas.view (in_channels, sigmas.shape[1],1, 1, 1, 1, 1, 1)
+        lambdas = lambdas.view(in_channels, 1, lambdas.shape[1],1, 1, 1, 1, 1)
+        gammas  = gammas.view (in_channels, 1, 1, gammas.shape[1], 1, 1, 1, 1)
+        psis    = psis.view (in_channels, 1, 1, 1, psis.shape[1], 1, 1, 1)
+
+        thetas  = thetas.view(1,1, 1, 1, 1, thetas.shape[0], 1, 1)
+        y       = y.view(1,1, 1, 1, 1, 1, y.shape[0], 1)
+        x       = x.view(1,1, 1, 1, 1, 1, 1, x.shape[0])
+
+        sigma_x = sigmas
+        sigma_y = sigmas / gammas
+
+        sin_t = torch.sin(thetas)
+        cos_t = torch.cos(thetas)
+        y_theta = -x * sin_t + y * cos_t
+        x_theta =  x * cos_t + y * sin_t
+        
+
+
+        gb = torch.exp(-.5 * (x_theta ** 2 / sigma_x ** 2 + y_theta ** 2 / sigma_y ** 2)) \
+             * torch.cos(2.0 * math.pi  * x_theta / lambdas + psis)
+
+        gb = gb.view(-1,kernel_size[0], kernel_size[1])
+
+        return gb
+
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        sy = x.size(2)
+        sx = x.size(3)  
+        gb = self.make_gabor_filters()
+
+        assert gb.shape[0] == self._n_channels_post_conv
+        assert gb.shape[1] == self.kernel_size
+        assert gb.shape[2] == self.kernel_size
+        gb = gb.view(self._n_channels_post_conv,1,self.kernel_size,self.kernel_size)
+
+        res = nn.functional.conv2d(input=x, weight=gb,
+            padding=self.kernel_radius, groups=self.in_channels)
+       
+        
+        if self.rotation_invariant:
+            res = res.view(batch_size, self.in_channels, -1, self.n_thetas,sy, sx)
+            res,_ = res.max(dim=3)
+
+        res = res.view(batch_size, -1,sy, sx)
+
+
+        return res
+
+class ComplexCell(nn.Module):
+
+    def __init__(self, in_channels):
+        super(ComplexCell, self).__init__()
+        self.in_channels = in_channels
+        self.gfb = GaborFilterBank(in_channels=self.in_channels, l_filter_size=16,
+                                       l_theta=15, l_sfs=2, l_phase=3, stride=4,
+                                       padding_mode='zeros',
+                                       contrast=1.).to('cuda')
+
+    
+    def forward(self, x):
+        r1 = self.gfb(x)
+
+        summation = r1
+
+        return summation
+        
+        
